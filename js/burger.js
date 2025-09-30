a44a66ca98289dc8a9ad250b07d240cfb86acbde
Vanxelsing123 <specnaz_790@mail.ru>
2025-09-30 13:41:42 +0300

    add

--- js/burger.js
+++ js/burger.js
@@ -5,5 +5,41 @@ const body = document.body
 burger.addEventListener('click', () => {
 	burger.classList.toggle('active')
 	nav.classList.toggle('active')
-	body.classList.toggle('no-scroll')
+
+	if (nav.classList.contains('active')) {
+		body.classList.add('no-scroll')
+	} else {
+		body.classList.remove('no-scroll')
+	}
+})
+
+const header = document.querySelector('.header')
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 50) {
+		header.classList.add('scrolled')
+	} else {
+		header.classList.remove('scrolled')
+	}
+})
+
+const links = document.querySelectorAll('.header__link')
+
+links.forEach(link => {
+	link.addEventListener('click', e => {
+		const href = link.getAttribute('href')
+
+		if (href.startsWith('#')) {
+			e.preventDefault()
+			const target = document.querySelector(href)
+			if (target) {
+				target.scrollIntoView({ behavior: 'smooth' })
+			}
+
+			// закрываем меню после клика
+			document.querySelector('.navigation').classList.remove('active')
+			document.querySelector('.burger').classList.remove('active')
+			document.body.classList.remove('no-scroll')
+		}
+	})
 })
